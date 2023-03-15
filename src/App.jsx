7eddebb1fb1ce6d3f2fd2285c1ad8e02c611f91b
Vickkk <justusofnigeria@gmail.com>
2023-03-15 11:32:15 +0100

    Project initialization

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+	return (
+		<div className="App">
+			<div className="bg-red-300">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam perferendis doloremque dolorem eos nihil vel aperiam vero ratione!
+				Assumenda aspernatur quas ea corporis ipsam maxime cupiditate error commodi, aliquam odio.
+			</div>
+		</div>
+	);
+}
+
+export default App;
